fe6b03c47424f7aa54b16f8f9498c508886bfc63
iostate <qmtruong92@gmail.com>
2023-01-24 23:51:58 -0700

    Main.rs is the echo tokio client

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
-use std::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
-// constants
-const SIROCCO_SERVER_ADDRESS: &str = "127.0.0.1:8000";
-fn main() {
-    // bind
-    let listener = TcpListener::bind(SIROCCO_SERVER_ADDRESS);
+const ECHO_SERVER_ADDRESS: &str = "localhost:8000";
+#[tokio::main]
+async fn main() {
+    // connection
+    println!("connecting to {}", ECHO_SERVER_ADDRESS);
+    if let Ok(mut stream) = TcpStream::connect(ECHO_SERVER_ADDRESS).await {
+        println!(
+            "connected to echo server {}:{} ... ",
+            stream.local_addr().unwrap().ip(),
+            stream.local_addr().unwrap().port()
+        );
 
-    // start
-    println!("sirocco listening {}", SIROCCO_SERVER_ADDRESS);
+        // write hello  world
+        let message = "Hello world";
+        // let _ = stream.write_all(message.as_bytes()).await;
+        let _ = stream.write_all(message.as_bytes()).await;
+        println!("sent: {}", message);
 
-    for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        // accept user input
+        // let mut line = String::new();
+        // println!("Enter a message: ");
+        // let b1 = std::io::stdin().read_line(&mut line).unwrap();
+        // let _ = stream.write(line.as_bytes());
 
-        println!("connection established!");
+        // read the result
+        let mut buffer = [0; 1024];
+        // let len = stream.read(&mut buffer).await.unwrap();
+        let len = stream.read(&mut buffer).await.unwrap();
+        let message = String::from_utf8_lossy(&buffer);
+        println!("received: {}", message);
+    } else {
+        println!("failed to connect to echo server {}", ECHO_SERVER_ADDRESS);
     }
 }
